432ead159ab80bfac5c7fc4e36295a3968d95c4c
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-07-03 12:41:04 -0700

    commit

--- sosad_download.py
+++ sosad_download.py
@@ -3,26 +3,33 @@
 import cached_url
 from bs4 import BeautifulSoup
 from telegram_util import compactText
+import os
+
+with open('cookie') as f:
+	cookie = f.read()
 
 def findLinks(soup):
 	for x in soup.find_all('a', class_='sosad-button'):
 		yield x['href']
 
-def getSoup(url, force_cache = True):
-	return BeautifulSoup(cached_url.get(
-		url, force_cache = force_cache), 'html.parser')
+def getSoup(url, force_cache = True, cookie = None):
+	return BeautifulSoup(cached_url.get(url, {'cookie': cookie}, 
+		force_cache = force_cache), 'html.parser')
+
+def getMainContent(link, cookie = None):
+	sub_soup = getSoup(link, force_cache = True, cookie = cookie)
+	return sub_soup.find('div', class_='main-text')
 
 def getText(link):
-	sub_soup = getSoup(link, force_cache = True)
-	main_content = sub_soup.find('div', class_='main-text')
+	main_content = getMainContent(link, cookie = None)
 	if not main_content:
-		print(link)
-		return ''
+		os.system('rm %s' % cached_url.getFilePath(link))
+		main_content = getMainContent(link, cookie = cookie)
 	for att in ['font-4', 'text-left']:
 		item = main_content.find('div', class_=att)
 		if item:
 			item.decompose()
-	return sub_soup.find('div', class_='main-text').get_text(separator='\n')
+	return main_content.get_text(separator='\n')
 
 def download(url, force_cache = False):
 	soup = getSoup(url, force_cache = force_cache)
@@ -34,4 +41,4 @@ def download(url, force_cache = False):
 		f.write(compactText(''.join(result)))
 	
 if __name__ == "__main__":
-	download('https://sosad.fun/threads/64862/profile', force_cache = True)
+	download('https://sosad.fun/threads/5968/profile', force_cache = True)
